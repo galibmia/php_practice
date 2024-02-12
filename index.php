0fe252fb824968d45605c27b0b30821b8523ad9f
Galib Mia <72703797+galibmia@users.noreply.github.com>
2024-02-12 18:56:18 +0600

    Practice array_map, array_walk, array_filter, pow() and some function

--- index.php
+++ index.php
@@ -163,3 +163,119 @@ function factorial(int $n){
 $number = 5;
 $result = factorial($number);
 echo "The Factorial of {$number} is {$result}.";
+
+
+echo PHP_EOL;
+// Nested Array \\ Multidimensional Array.
+
+$students = [
+    'name'=> ['Galib', 'Rostom', 'Tarikul'],
+    'roll' => [12, 13, 15, 13],
+    'home' => [
+        ['house'=> 13, 'road'=>17, 'Thana'=>'Mirpur', 'District'=> 'Dhaka'],
+        ['house'=> 25, 'road'=>07, 'Thana'=>'Uttara', 'District'=> 'Dhaka'],
+        ['house'=> 25, 'road'=>18, 'Thana'=>'Mohammadpur', 'District'=> 'Dhaka']
+        ]
+];
+
+$tarikulAddress = $students['home'][2]['Thana'];
+echo $tarikulAddress;
+
+
+echo PHP_EOL;
+
+// Array sorting: 
+/**
+ * sort($arrayName) --- using this, sort by value.
+ * rsort($arrayName) --- sort by value reverse.
+ * asort($arrayName) --- sort by value with contain index/key. 
+ * arsort($arrayName) --- sort by value with contain index/key reverse. 
+ * ksort($arrayName) --- sort by index/key value. 
+ * krsort($arrayName) --- sort by index/key value reverse. 
+ */
+
+$number = [12, 45, 16, 89 ,11, 10];
+
+sort($number); 
+print_r($number);
+
+
+echo PHP_EOL;
+/** Array Search:
+*   in_array(Element, arrayName): Check element exist or not.
+*   array_search(Element, arrayName):if exist, return index number 
+*/
+
+$searchingElement = 11;
+
+if(in_array($searchingElement, $number)){ // Array Search: Check element exist or not. 
+    $offset = array_search($searchingElement, $number); // Array Search:if exist, return index number
+    printf("{$searchingElement} is found in index number {$offset}");
+}else{
+    echo "Not found";
+}
+
+
+echo PHP_EOL;
+/** Array Common Element Search:
+*   array_intersect(arrayName, arrayName): Check the element of First array are exist in the Second array. Return matched value/element from First array.
+
+*  array_intersect_assoc(arrayName, arrayName): Check the element and also the index/key of First array are exist in the Second array. Return matched value/element and index/key from First array.
+
+* array_diff(arrayName, arrayName): Check the element of First array are not exist in the Second array. Return not matched value/element from First array.
+
+* array_diff(arrayName, arrayName): Check the element and index\key of First array are not exist in the Second array. Return not matched value/element with index/key from First array.
+*/
+
+$arrayOne = [15, 10, 26, 24, 25];
+$arrayTwo = [16, 10, 21, 26, 25];
+
+echo "Matched value from arrayOne \n";
+print_r(array_intersect($arrayOne, $arrayTwo));
+echo "Matched value and index from arrayOne  \n";
+print_r(array_intersect_assoc($arrayOne, $arrayTwo));
+echo "Not matched value from arrayOne  \n";
+print_r(array_diff($arrayOne, $arrayTwo));
+echo "Not matched value and index from arrayOne \n";
+print_r(array_diff_assoc($arrayOne, $arrayTwo));
+
+
+
+
+/**
+ * array_walk(arrayName, 'functionName'); --- To work with every element of an array and print it. 
+ * array_map('functionName', arrayName); --- To work with every element of an array and return it. 
+ * array_filter(arrayName, 'functionName') --- work with every element, check and return filtered element. 
+ * 
+ */
+
+    // Make square and Print //No return
+    function square($number){
+        
+        printf("The square of %d is %d \n", $number, pow($number, 2));
+    }
+
+    // Make cube and return
+    function cube($number){
+        return pow($number, 3);
+    }
+    // check a name is start with 'S' or not. if true, then return the name.
+    function findByS($name){
+        return $name[0]=="S";
+    }
+
+
+ $array = [1, 2, 3, 4, 5, 6];
+ $names = ['Sadia', 'Galib', 'Suma', 'Tarikul', 'Rostom', 'Sompa'];
+
+ array_walk($array, 'square');
+
+ echo PHP_EOL;
+
+ $newArray =  array_map('cube', $array);
+ print_r($newArray);
+
+ echo PHP_EOL;
+ 
+ $newNames = array_filter($names, 'findByS');
+ print_r($newNames);
